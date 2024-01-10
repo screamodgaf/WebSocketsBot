183f3bfeda80dcbdc91bcb5d9526704bf9d77f8d
screamodgaf <80184783+screamodgaf@users.noreply.github.com>
2024-01-10 14:40:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::string nick = "ProtoBrunetka";
-    std::string room = "grunge";
+    std::string nick = "Proto";
+    std::string room = "Chatroom";
 
     Client client(QUrl(QStringLiteral("wss://s1.polfan.pl:16080")), nick, room);
 
